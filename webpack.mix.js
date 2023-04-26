ead5fb17194e9b09e5218e069ec99f200a1e45b9
miftahurrahmi <miftahurrahmi45@gmail.com>
2023-04-26 09:27:32 +0700

    install badaso

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,8 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+// Badaso
+mix.js("vendor/badaso/core/src/resources/js/app.js", "public/js/badaso.js")
+    .sass("vendor/badaso/core/src/resources/js/assets/scss/style.scss", "public/css/badaso.css")
+    .vue()
